6a93cd407aa803c7ec17bd4775d5e1d6ff39f2bb
ButItDoesRun <90749391+ButItDoesRun@users.noreply.github.com>
2022-12-02 14:01:27 +0100

    git ignore changed back

---
+++ src/page_components/StateMachine.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const StateMachine = ({state,setState},{val,setVal}, ){
+    return(
+
+        <p>test</p>
+
+
+
+
+    );
+
+}
+
+    
+export default StateMachine;
