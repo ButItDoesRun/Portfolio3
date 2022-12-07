b3cc6440a1051785c3e7c9ab36a2999bf71bba60
Anne-Thea <annethearendtorff@gmail.com>
2022-12-07 17:05:21 +0100

    Added edit page for bookmarks

---
+++ src/Pages/BookmarksEditPage.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useContext, useState } from 'react';
+import { useParams, useNavigate } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import Bookmark from '../DataService/Bookmark';
+import BookmarkForm from '../PageComponents/BookmarksPageComponents/BookmarkForm';
+import TokenContext from '../Context/TokenContext';
+
+async function UpdateBookmark(token, id, name) {
+    const bookmark = new Bookmark(id, name);
+    const result = await bookmark.UpdateBookmark(token);
+    return (result);
+};
+
+const BookmarksEditPage = () => {
+    const navigate = useNavigate();
+    const { id } = useParams();
+    const { name } = useParams();
+    let [editName, setEditName] = useState(name);
+    let [bookmarkEdited, setBookmarkEdited] = useState(null);
+    const token = useContext(TokenContext);
+
+    useEffect(() => {
+        if (bookmarkEdited === false) {
+            alert("Error: Bookmark was not edited");
+            // navigate("/user/bookmarks");
+        }
+        if (bookmarkEdited === true) {
+            navigate("/user/bookmarks");
+        }
+    }, [bookmarkEdited]);
+
+    return (
+        <Container fluid>
+            <Row>
+                <BookmarkForm name={editName} setName={setEditName}></BookmarkForm>
+            </Row>
+            <Row>
+                <Col>
+                    <Button variant="primary" onClick={async () => {
+                        const result = await UpdateBookmark(token, id, editName);
+                        setBookmarkEdited(result);
+                    }}>Rename bookmark</Button>
+                </Col>
+                <Col>
+                <Button variant="danger" onClick={() => {
+                        setBookmarkEdited(false);
+                        navigate("/user/bookmarks");
+                    }}>Cancel</Button>
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+
+export default BookmarksEditPage;
