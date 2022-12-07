d93f1cc1d19bc7e7e006ab86f53b471662d36ae5
ButItDoesRun <90749391+ButItDoesRun@users.noreply.github.com>
2022-12-07 13:53:26 +0100

    fixed create bookmark in personById

--- src/PageComponents/PersonPageComponents/PersonById.jsx
+++ src/PageComponents/PersonPageComponents/PersonById.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
+import {NavLink} from "react-router-dom";
 
 // Components
 // import DirectorList from './DirectorList';
@@ -9,30 +10,36 @@ import Tabs from 'react-bootstrap/Tabs';
 import ProfessionList from './ProfessionList';
 import KnownForTitlesList from "./KnownForTitlesList";
 
-const PersonById = ({ person }) =>
-    <Container fluid>
-        <h2>{person.name}</h2>
-        <Tabs
-            defaultActiveKey="facts"
-            id="uncontrolled-tab-example"
-            className="mb-3"
-        >
-            <Tab eventKey="facts" title="Facts">
-                <p>Birth Year: {person.birthYear}</p>
-                <p>Death Year: {person.deathYear}</p>
-                <p><a href={person.bookmark}> Bookmark Me!</a></p>
-            </Tab>
+const PersonById = ({ person }) => {
+    const lastSegment = person.bookmark.split("/").pop();
+    const url = `/user/bookmarks/create/${lastSegment}`;
+    return (
+        <Container fluid>
+            <h2>{person.name}</h2>
+            <Tabs
+                defaultActiveKey="facts"
+                id="uncontrolled-tab-example"
+                className="mb-3"
+            >
+                <Tab eventKey="facts" title="Facts">
+                    <p>Birth Year: {person.birthYear}</p>
+                    <p>Death Year: {person.deathYear}</p>
+                    <p><NavLink to={url} 
+                    onClick = { () => window.location.replace(url)}>Bookmark Me!</NavLink></p>
+                </Tab>
 
-            <Tab eventKey="professions" title="Professions">
-                <ProfessionList professionList={person.professionList}></ProfessionList>
-            </Tab>
+                <Tab eventKey="professions" title="Professions">
+                    <ProfessionList professionList={person.professionList}></ProfessionList>
+                </Tab>
 
-            <Tab eventKey="knownFor" title="Known For">
-                {person.knownForListWithUrl.length === 0 ? <p>Not famous for any movies starred in</p>
-                : <KnownForTitlesList knownForTitlesList = {person.knownForListWithUrl}></KnownForTitlesList>
-                }
-            </Tab>
-        </Tabs>
-    </Container>;
+                <Tab eventKey="knownFor" title="Known For">
+                    {person.knownForListWithUrl.length === 0 ? <p>Not famous for any movies starred in</p>
+                    : <KnownForTitlesList knownForTitlesList = {person.knownForListWithUrl}></KnownForTitlesList>
+                    }
+                </Tab>
+            </Tabs>
+        </Container>
+    );
+};
 
 export default PersonById;
