0efe13a6779d40639521bb9a4c93184fdd74ccdd
Anne-Thea <annethearendtorff@gmail.com>
2022-12-07 16:18:54 +0100

    added buttons to bookmarks for delete and edit

---
+++ src/PageComponents/BookmarksPageComponents/BookmarkWithButtons.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Container from "react-bootstrap/Container";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import { Link } from "react-router-dom";
+import Bookmark from './Bookmark';
+
+const BookmarkWithButtons = ({ bookmark }) => {
+    const lastSegment = bookmark.url.split("/").pop();
+    return (
+        <Container fluid>
+            <Row>
+                <Col>
+                    <Bookmark bookmark={bookmark} />
+                </Col>
+                <Col md="auto">
+                    <Button variant="primary" as={Link} to={"/user/bookmarks/edit/" + lastSegment}>
+                        Edit
+                    </Button>
+                </Col>
+                <Col md="auto">
+                    <Button variant="danger" as={Link} to={"/user/bookmarks/delete/" + lastSegment}>
+                        Delete
+                    </Button>
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+
+export default BookmarkWithButtons;
--- src/PageComponents/BookmarksPageComponents/BookmarksList.jsx
+++ src/PageComponents/BookmarksPageComponents/BookmarksList.jsx
@@ -1,31 +1,17 @@
 import React from 'react';
 import Container from "react-bootstrap/Container";
 import ListGroup from 'react-bootstrap/ListGroup';
-import { NavLink, Link } from "react-router-dom";
-
-const Bookmark = ({ bookmark }) => {
-    const lastSegment = bookmark.url.split("/").pop();
-    let url;
-    if (lastSegment.includes("t")) {
-        url = "/title/"+lastSegment;
-    } else if(lastSegment.includes("n")){
-        url = "/person/"+lastSegment;
-    }
-
-    return(
-        <NavLink as={Link} to={url}>{bookmark.name}</NavLink>
-    );
-};
+import BookmarkWithButtons from './BookmarkWithButtons';
 
 const BookmarksList = ({ bookmarkList }) =>
-    <Container fluid>
-        <ListGroup>
-            {bookmarkList.map(bookmark =>
-                <ListGroup.Item key={bookmark.name}>
-                    <Bookmark bookmark={bookmark} />
-                </ListGroup.Item>
-            )}
-        </ListGroup>
-    </Container>;
+<Container fluid>
+    <ListGroup>
+        {bookmarkList.map(bookmark =>
+            <ListGroup.Item key={bookmark.name}>
+                <BookmarkWithButtons bookmark={bookmark} />
+            </ListGroup.Item>
+        )}
+    </ListGroup>
+</Container>;
 
 export default BookmarksList;
