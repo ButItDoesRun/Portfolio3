8abff2cc559ce584d250cf8560c6973066a40872
Anne-Thea <annethearendtorff@gmail.com>
2022-12-16 10:06:34 +0100

    fixed keys

--- src/PageComponents/HistoryPageComponents/HistoryList.jsx
+++ src/PageComponents/HistoryPageComponents/HistoryList.jsx
@@ -3,11 +3,16 @@ import Container from "react-bootstrap/Container";
 import ListGroup from 'react-bootstrap/ListGroup';
 import History from './History';
 
+function generateKey(){
+    const randomKey = Math.floor(Math.random() * 100);
+    return(randomKey);
+}
+
 const HistoryList = ({ historyList }) =>
     <Container fluid>
         <ListGroup>
             {historyList.map(history =>
-                <ListGroup.Item key={history.content}>
+                <ListGroup.Item key={history.date + generateKey()}>
                     <History history={history} />
                 </ListGroup.Item>
             )}
